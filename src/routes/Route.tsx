e05bc7d991b28a2b2ba7a302add8f77528b7c89e
muhammadali276397 <muhammadali246397@gmail.com>
2024-11-12 12:52:50 +0600

    making routes and page route design

---
+++ src/routes/Route.tsx
@@ -0,0 +1,17 @@
+
+import MainLayout from "@/layout/MainLayout";
+import Home from "@/pages/Home";
+import { createBrowserRouter } from "react-router-dom";
+
+export const router = createBrowserRouter([
+    {
+        path: "/" ,
+        element: <MainLayout/>,
+        children:[
+            {
+                index:true,
+                element: <Home/>
+            }
+        ]
+    }
+])
