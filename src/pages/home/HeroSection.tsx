6ae09ce221c705b19de3876f0b78e721791d1edf
muhammadali276397 <muhammadali246397@gmail.com>
2024-11-13 18:59:11 +0600

    making a hero section with responsive

--- src/pages/home/HeroSection.tsx
+++ src/pages/home/HeroSection.tsx
@@ -1,11 +1,45 @@
+
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation } from 'swiper/modules';
+import img1 from "./../../assets/img1.jpg"
+import img2 from "./../../assets/img2.jpg"
+import img3 from "./../../assets/img3.jpg"
 import Container from '@/lib/Container';
+import { Button } from '@/components/ui/button';
 
 const HeroSection = () => {
     return (
         <div>
-            <Container classname={"bg-green-500"}>
-                <h1>this is hero section</h1>
-            </Container>
+            <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+        <SwiperSlide className='w-full' style={{backgroundImage :`url(${img1})`,backgroundSize:'cover' }}>
+            <div className='w-full py-28 bg-black bg-opacity-40'>
+                <Container classname={'text-center'}>
+                <h1 className='text-7xl lg:px-10 font-bold text-white leading-[90px]'>Vulputate Mollis Ultricies Fermentum Parturient</h1>
+                <Button className='mt-10 bg-[#5191FA] text-white text-lg font-semibold'>Read More</Button>
+                </Container>
+            </div>
+        </SwiperSlide>
+        <SwiperSlide className='w-full' style={{backgroundImage :`url(${img2})`,backgroundSize:'cover' }}>
+            <div className='w-full py-28 bg-black bg-opacity-40'>
+                <Container classname={'text-center'}>
+                <h1 className='text-7xl lg:px-10 font-bold text-white leading-[90px]'>Vulputate Mollis Ultricies Fermentum Parturient</h1>
+                <Button className='mt-10 bg-[#5191FA] text-white text-lg font-semibold'>Read More</Button>
+                </Container>
+            </div>
+        </SwiperSlide>
+        <SwiperSlide className='w-full' style={{backgroundImage :`url(${img3})`,backgroundSize:'cover' }}>
+            <div className='w-full py-28 bg-black bg-opacity-40'>
+                <Container classname={'text-center'}>
+                <h1 className='text-7xl lg:px-10 font-bold text-white leading-[90px]'>Vulputate Mollis Ultricies Fermentum Parturient</h1>
+                <Button className='mt-10 bg-[#5191FA] text-white text-lg font-semibold'>Read More</Button>
+                </Container>
+            </div>
+        </SwiperSlide>
+        
+      </Swiper>
         </div>
     );
 };
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,12 +1,12 @@
-import Container from "@/lib/Container";
+
 import HeroSection from "./HeroSection";
 
 
 const Home = () => {
     return (
-      <Container>
+     
         <HeroSection/>
-      </Container>
+     
     );
 };
 
